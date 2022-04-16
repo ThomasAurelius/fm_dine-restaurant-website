a64d3151bb31f2df91a8725d3f50afcb4370a18f
Thomas Aurelius <45251987+ThomasAurelius@users.noreply.github.com>
2022-04-15 23:09:56 -0500

    still messing with js

--- index.js
+++ index.js
@@ -24,54 +24,43 @@ minus.addEventListener('click', (e) => {
 
 // family gathering section
 
-const tabList = document.querySelector('[role="tablist"]');
-const tabs = document.querySelectorAll('[role="tab"]');
+// const tabList = document.querySelector('[role="tablist"]');
+// const tabs = tabList.querySelectorAll('[role="tab"]');
 
-tabs.forEach((tab) => {
-    tab.addEventListener('click', () => {
-      console.log('fuck')
-    });
-});
+// tabs.forEach((tab) => {
+//     tab.addEventListener('click', changeTabPanel);
+// });
 
-function changeTabPanel(e) {
-    const targetTab = e.target;
-    const targetPanel = targetTab.getAttribute("aria-controls");
+// function changeTabPanel(e) {
+//     const targetTab = e.target;
+//     const targetPanel = targetTab.getAttribute("aria-controls");
     
-    console.log(targetTab);
-}
+//     const tabContainer = targetTab.parentNode;
+//     const mainContainer = tabContainer.parentNode;
+    
+//     mainContainer
+//         .querySelectorAll('article')
+//         .forEach((article) => article.setAttribute("hidden", true));
+    
+//     mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    
 
 
 
 
 
 
+const family = document.getElementById('family-tab')
+const special = document.getElementById('special-tab')
+const social = document.getElementById('social-tab')
 
-// const family = document.getElementById('family-article')
-// const special = document.getElementById('special-article')
-// const social = document.getElementById('social-event')
+const familyBtn = document.getElementById('family-btn')
+const specialBtn = document.getElementById('special-btn')
+const socialBtn = document.getElementById('social-btn')
 
-// const familyBtn = document.getElementById('family')
-// const specialBtn = document.getElementById('special')
-// const socialBtn = document.getElementById('social')
-
-// specialBtn.addEventListener('click', (e) => {
-//    e.preventDefault()
-//    family.setAttribute("hidden", true)
-//    special.setAttribute("hidden", false)
-// }
-
-
-
-
-
-
-
-
-
-
-// Special Events
-// Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you.
-// We’ll be sure to mark your special date with an unforgettable meal.
-// Social Events
-// Are you looking to have a larger social event? No problem! We’re more than happy to cater for big
-// parties. We’ll work with you to make your event a hit with everyone.
+specialBtn.addEventListener('click', (e) => {
+   e.preventDefault()
+   alert(e)
+   family.setAttribute("hidden", true)
+   special.removeAttribute("hidden")
+})
